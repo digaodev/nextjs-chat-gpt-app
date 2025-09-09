92cd6c272f084033f8d68027390ad4ccb1e92c38
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2025-09-09 19:54:37 -0300

    feat: add openai and Chat messages and completions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import { Separator } from "@/components/ui/separator";
+import Chat from "./components/Chat";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  const isLoggedIn = session?.user?.name || session?.user?.email;
+
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
+
+      {isLoggedIn ? (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      ) : (
+        <div>You need to log in to use the chat</div>
+      )}
     </main>
   );
 }
---
+++ src/app/server-actions/getCompletion.ts
@@ -0,0 +1,28 @@
+"use server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+interface Message {
+  role: "user" | "assistant";
+  content: string;
+}
+
+export async function getCompletion(messageHistory: Message[]) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: messageHistory,
+  });
+
+  const messages = [
+    ...messageHistory,
+    response.choices[0].message as unknown as {
+      role: "user" | "assistant";
+      content: string;
+    },
+  ];
+
+  return { messages };
+}
