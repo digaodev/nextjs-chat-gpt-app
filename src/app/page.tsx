e281783132a85963ad652faf752fdc297649070a
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2025-09-17 16:55:17 -0300

    feat: show previous chats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
+import { getServerSession } from "next-auth";
+
 import { Separator } from "@/components/ui/separator";
 import Chat from "./components/Chat";
-import { getServerSession } from "next-auth";
+import PreviousChats from "./components/PreviousChats";
+import { Suspense } from "react";
 
 export default async function Home() {
   const session = await getServerSession();
-  const isLoggedIn = session?.user?.name || session?.user?.email;
 
   return (
     <main className="p-5">
-      <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
-
-      {isLoggedIn ? (
+      <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+      {!session?.user?.email && <div>You need to log in to use this chat.</div>}
+      {session?.user?.email && (
         <>
+          <Suspense fallback={<div>Loading previous chats...</div>}>
+            <PreviousChats />
+          </Suspense>
+          <h4 className="mt-5 text-2xl font-bold">New Chat Session</h4>
           <Separator className="my-5" />
           <Chat />
         </>
-      ) : (
-        <div>You need to log in to use the chat</div>
       )}
     </main>
   );
